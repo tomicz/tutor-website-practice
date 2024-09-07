b45921f61f39e00d79129bc300ee21b38b094d3a
Tomicz Engineering LLC <tomiczdarko@gmail.com>
2024-09-07 02:52:42 +0200

    Added dissapearing navbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Toggle mobile menu
 document.addEventListener("DOMContentLoaded", function(){
     const toggleButton = document.querySelector(".navbar .mobile-menu-toggle");
     const mobileMenu = document.querySelector(".navbar .mobile-menu-items");
@@ -5,4 +6,17 @@ document.addEventListener("DOMContentLoaded", function(){
     toggleButton.addEventListener("click", function(){
         mobileMenu.classList.toggle("active");
     });
+});
+
+// Change navbar background on scroll
+window.addEventListener("scroll", function(){
+    const scrollY = this.window.scrollY; 
+    const navbar = this.document.querySelector(".navbar");
+
+    if(scrollY > 0){
+        navbar.classList.add("navbar-scroll");
+    }
+    else{
+        navbar.classList.remove("navbar-scroll");
+    }
 });
